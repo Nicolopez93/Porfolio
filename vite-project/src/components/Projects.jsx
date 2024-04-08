469c0b8efd7c9bc2a0516a6697e7b395d518a79f
Nicolopez93 <nicolopezarq@gmail.com>
2024-04-08 17:01:35 -0300

    porfolio hasta el dia 8/4

--- vite-project/src/components/Projects.jsx
+++ vite-project/src/components/Projects.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import ProjectsItems from './ProjectsItems'
-import img from '../assets/codigo-programacion-desarrollo-web-o-desarrollo-aplicaciones-diseno-web-computadora-fondo-seo-3d_125322-458.avif'
+import React from "react";
+import ProjectsItems from "./ProjectsItems";
+import img from "../assets/codigo-programacion-desarrollo-web-o-desarrollo-aplicaciones-diseno-web-computadora-fondo-seo-3d_125322-458.avif";
 const Projects = () => {
   return (
     <div id="project" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-      <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
-      <p className='text-center py-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod neque reprehenderit ipsum, eligendi in quos? Doloribus quidem porro eveniet laborum quis, optio nulla deleniti esse dolorum dignissimos cumque, rem accusamus.</p>
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">
+        Projects
+      </h1>
+      <p className="text-center py-8">
+        During my projects, I've learned technologies such as HTML, CSS,
+        JavaScript, React, Vite, Postman, H2, MySQL, Java, among
+        others. These skills have allowed me to develop both in frontend and
+        backend. Below, I share some of the projects I've worked on:
+      </p>
       <div className="grid sm:grid-cols-2 gap-12">
-        <ProjectsItems img={img} title="Frontend 1" p = "Html y Css"/>
-        <ProjectsItems img={img} title="Frontend 2" p = "javascript"/>
-        <ProjectsItems img={img} title="Frontend 3" p = "React y Vite"/>
-        <ProjectsItems img={img} title="Car4All" p = "React y Vite"/>
-        <ProjectsItems img={img} title="Backend 1" p = "java H2 Postman"/>
-        <ProjectsItems img={img} title="Backend 2" p = "java H2 Postman"/>
+        <ProjectsItems img={img} title="Frontend 1" p="Html y Css" />
+        <ProjectsItems img={img} title="Frontend 2" p="javascript" />
+        <ProjectsItems img={img} title="Frontend 3" p="React y Vite" />
+        <ProjectsItems img={img} title="Car4All" p="React y Vite" />
+        <ProjectsItems img={img} title="Backend 1" p="java H2 Postman" />
+        <ProjectsItems img={img} title="Backend 2" p="java H2 Postman" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- vite-project/src/components/Work.jsx
+++ vite-project/src/components/Work.jsx
@@ -6,35 +6,35 @@ const data = [
     title: "Certified Tech Developer en Digital House",
     duration: "2 Years",
     details:
-      "Durante este programa de dos años en Digital House, he adquirido una amplia gama de habilidades técnicas y conocimientos fundamentales que me han preparado para enfrentar los desafíos del mundo tecnológico actual. He desarrollado competencias en áreas como desarrollo web, programación, metodologías ágiles y más. Además, he tenido la oportunidad de trabajar en proyectos prácticos y colaborativos que han fortalecido mi capacidad para resolver problemas y trabajar en equipo.",
+      "During this two-year program at Digital House, I have acquired a wide range of technical skills and fundamental knowledge that have prepared me to tackle the challenges of the current technological world. I have developed competencies in areas such as web development, programming, agile methodologies, and more. Additionally, I have had the opportunity to work on practical and collaborative projects that have strengthened my ability to solve problems and work in teams",
   },
-  {
-    year: "2022-2024",
-    title: "Certified Tech Developer en Digital House",
-    duration: "2 Years",
-    details:
-      "Durante este programa de dos años en Digital House, he adquirido una amplia gama de habilidades técnicas y conocimientos fundamentales que me han preparado para enfrentar los desafíos del mundo tecnológico actual. He desarrollado competencias en áreas como desarrollo web, programación, metodologías ágiles y más. Además, he tenido la oportunidad de trabajar en proyectos prácticos y colaborativos que han fortalecido mi capacidad para resolver problemas y trabajar en equipo.",
-  },
-  {
-    year: "2022-2024",
-    title: "Certified Tech Developer en Digital House",
-    duration: "2 Years",
-    details:
-      "Durante este programa de dos años en Digital House, he adquirido una amplia gama de habilidades técnicas y conocimientos fundamentales que me han preparado para enfrentar los desafíos del mundo tecnológico actual. He desarrollado competencias en áreas como desarrollo web, programación, metodologías ágiles y más. Además, he tenido la oportunidad de trabajar en proyectos prácticos y colaborativos que han fortalecido mi capacidad para resolver problemas y trabajar en equipo.",
-  },
-  {
-    year: "2022-2024",
-    title: "Certified Tech Developer en Digital House",
-    duration: "2 Years",
-    details:
-      "Durante este programa de dos años en Digital House, he adquirido una amplia gama de habilidades técnicas y conocimientos fundamentales que me han preparado para enfrentar los desafíos del mundo tecnológico actual. He desarrollado competencias en áreas como desarrollo web, programación, metodologías ágiles y más. Además, he tenido la oportunidad de trabajar en proyectos prácticos y colaborativos que han fortalecido mi capacidad para resolver problemas y trabajar en equipo.",
-  },
+  // {
+  //   year: "2022-2024",
+  //   title: "Certified Tech Developer en Digital House",
+  //   duration: "2 Years",
+  //   details:
+  //     "Durante este programa de dos años en Digital House, he adquirido una amplia gama de habilidades técnicas y conocimientos fundamentales que me han preparado para enfrentar los desafíos del mundo tecnológico actual. He desarrollado competencias en áreas como desarrollo web, programación, metodologías ágiles y más. Además, he tenido la oportunidad de trabajar en proyectos prácticos y colaborativos que han fortalecido mi capacidad para resolver problemas y trabajar en equipo.",
+  // },
+  // {
+  //   year: "2022-2024",
+  //   title: "Certified Tech Developer en Digital House",
+  //   duration: "2 Years",
+  //   details:
+  //     "Durante este programa de dos años en Digital House, he adquirido una amplia gama de habilidades técnicas y conocimientos fundamentales que me han preparado para enfrentar los desafíos del mundo tecnológico actual. He desarrollado competencias en áreas como desarrollo web, programación, metodologías ágiles y más. Además, he tenido la oportunidad de trabajar en proyectos prácticos y colaborativos que han fortalecido mi capacidad para resolver problemas y trabajar en equipo.",
+  // },
+  // {
+  //   year: "2022-2024",
+  //   title: "Certified Tech Developer en Digital House",
+  //   duration: "2 Years",
+  //   details:
+  //     "Durante este programa de dos años en Digital House, he adquirido una amplia gama de habilidades técnicas y conocimientos fundamentales que me han preparado para enfrentar los desafíos del mundo tecnológico actual. He desarrollado competencias en áreas como desarrollo web, programación, metodologías ágiles y más. Además, he tenido la oportunidad de trabajar en proyectos prácticos y colaborativos que han fortalecido mi capacidad para resolver problemas y trabajar en equipo.",
+  // },
 ];
 
 const Work = () => {
   return (
     <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-      <h1 className="text-4xl font-bold text-center text-[#001b5e]">Work</h1>
+      <h1 className="text-4xl font-bold text-center text-[#001b5e] p-4">Work</h1>
       {data.map((item, idx) => (
         <WorkItem
           key={idx}
